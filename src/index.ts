6fbb7a94f00de29b4533fbd1a767075bff999891
santistebanc <santistebanc94@gmail.com>
2025-06-11 12:54:56 +0200

    first

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import shortHash from "short-hash";
+import { z } from "zod/v4";
+import { makeType } from "any-db"
+
+export const Airport = makeType('Airport', {
+    shape: {
+        iata: z.string().length(3)
+    },
+    id: ({ iata }) => iata,
+})
+
+export const Airline = makeType('Airline', {
+    shape: {
+        name: z.string()
+    },
+    id: ({ name }) => name,
+})
+
+export const Flight = makeType('Flight', {
+    shape: {
+        flightNumber: z.string(),
+        airline: Airline.schema,
+        from: Airport.schema,
+        to: Airport.schema,
+        date: z.iso.date(),
+    },
+    id: ({ flightNumber, date, from, to }) => `${flightNumber}-${date}-${from.iata}-${to.iata}`
+})
+
+export const Bundle = makeType('Bundle', {
+    shape: {
+        items: z.array(Flight.schema)
+    },
+    id: ({ items }) => shortHash(items.flat().map((i: any) => i.id).join(',')),
+})
+
+export const Deal = makeType('Deal', {
+    shape: {
+        price: z.number(),
+        dealer: z.string(),
+        source: z.string(),
+        target: z.union([Flight.schema, Bundle.schema]),
+        link: z.string(),
+        date: z.iso.date(),
+    },
+    id: ({ target, price, dealer, source }) => `${target.type}-${target.id}-${price}-${dealer}-${source}`
+})
---
+++ src/utils.ts
@@ -0,0 +1,55 @@
+import { isNode } from "any-db"
+
+export function getKey(obj: Record<string, any>): string {
+    if ('key' in obj) return obj.key
+    if (isNode(obj)) return `${obj.type}:${obj.id}`
+    throw 'cannot get key'
+}
+
+export type Store<Type> = {
+    put: (create: Type & { key?: string }, update?: (item: Type & { key?: string }) => void) => void,
+    list: () => (Type & { key?: string })[]
+}
+export function store<Type extends {}>(...initialItems: ((Type & { key?: string }) | (Type & { key?: string })[])[]): Store<Type> {
+    const memo: Record<string, any> = {}
+    initialItems.flat().forEach(it => memo[getKey(it)] = it)
+    function put(create: Type & { key?: string }, update?: (item: Type & { key?: string }) => void) {
+        if (getKey(create) in memo && update) {
+            update(memo[getKey(create)])
+        } else {
+            memo[getKey(create)] = create
+        }
+    }
+    function list() {
+        return Object.values(memo) as (Type & { key: string })[]
+    }
+    return { put, list }
+}
+
+export function hextime(date?: number) {
+    if (date === undefined) return Date.now().toString(36);
+    return date.toString(36);
+};
+
+export function setDeep(path: string[], value: any, obj: Record<string, any>) {
+    if (path[0] === undefined) throw 'path must contain at least 1 string'
+    if (path.length > 1) {
+        if (!(path[0] in obj)) obj[path[0]] = {}
+        setDeep(path.slice(1), value, obj[path[0]])
+    } else {
+        obj[path[0]] = value
+    }
+}
+
+export function getDeep(path: string[], obj: Record<string, any>) {
+    if (path[0] === undefined) return obj
+    if (!(path[0] in obj)) return undefined
+    return getDeep(path.slice(1), obj[path[0]])
+}
+
+export function flatObject(obj: Record<string, any>, checkContinue: (obj: Record<string, any>) => boolean, path: string[] = []): any[] {
+    if (typeof obj === 'object' && obj !== null && checkContinue(obj)) {
+        return Object.entries(obj).flatMap(([k, v]) => flatObject(v, checkContinue, [...path, k]))
+    }
+    return [{ path, value: obj }]
+}
